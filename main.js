826aada5a10a192bb8d9cbe04d6151ef315161a1
Shane D Wilson <me@shanedwilson.com>
2018-10-28 12:23:22 -0500

    capturing input

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('main')
+$( "#search" ).submit(function (event) {
+    let userInput = $(".form-control").val();
+    console.log('input: ', userInput);
+    event.preventDefault();
+   $( ".form-control" ).blur();
+   $('.form-control').val("");
+});
