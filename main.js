b1f70b0015f7acb5cf847a8031c9ea7ecb79fdb3
Shane D Wilson <me@shanedwilson.com>
2018-10-28 12:31:33 -0500

    wip

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 $( "#search" ).submit(function (event) {
     let userInput = $(".form-control").val();
-    console.log('input: ', userInput);
+    getBook(userInput);
     event.preventDefault();
    $( ".form-control" ).blur();
    $('.form-control').val("");
 });
+
+const getBook = (userInput) => {
+    return new Promise ((resolve, reject) => {
+        $.ajax(`'openlibrary.org/api/books?bibkeys=ISBN:${userInput}&format=json'`)
+    }).done(book => {
+        resolve(book);
+        console.log(book);
+    }) .fail (error => {
+        reject(error);
+    })
+}
